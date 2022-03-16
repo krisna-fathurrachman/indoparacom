32dcff53f0ae6c9ee6382f47eb1563c7b2feb44d
krisna-fathurrachman <56065663+lordkrisna@users.noreply.github.com>
2022-03-16 17:03:26 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bgimg {
+  background-color: #000000;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: #fff;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 50px;
+}
+
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 50px;
+}
+
+.middle {
+  position: absolute;
+  font-size:45px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 50%;
+}
+</style>
+<body>
+
+<div class="bgimg">
+  <div class="topleft">
+    <img src="img/indopara.jpg" alt="logo" style="width:130px;height:130px;">
+    <p></p>
+  </div>
+  <div class="middle">
+    <h1>COMING SOON</h1>
+    <hr>
+    
+  </div>
+  <div class="bottomleft">
+    
+  </div>
+</div>
+
+</body>
+</html>
