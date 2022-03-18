707a5995387011cb1043b0ad63ca84819add2c26
Anggiawanprasetyo <aanggiawan92@gmail.com>
2022-03-18 16:07:27 +0700

    Update newberanda.blade.php

--- resources/views/newberanda.blade.php
+++ resources/views/newberanda.blade.php
@@ -24,9 +24,9 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
                     <a class="nav-link" href="#">Beranda</a>
-                  <a class="nav-link" href="#">Tentang Kami</a>
+                  <a class="nav-link" href="#about-section">Tentang Kami</a>
                   <a class="nav-link" href="#">Produk</a>
-                  <a class="nav-link" href="#">Kontak</a>
+                  <a class="nav-link" href="#footer">Kontak</a>
                     </ul>
                 </div>
             </div>
@@ -37,11 +37,53 @@
                 <div class="container px-5">
                     <h1 class="masthead-heading mb-0">PT INDO PARA SISTA SEMESTA</h1>
                     <h2 class="masthead-subheading mb-6">MILITARY EQUIPMENT</h2>
-                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Read More</a>
+                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#about-section">Read More</a>
                 </div>
             </div>
             
         </header>
+
+        <!--about-->
+        {{-- <div class="site-section" id="about-section">
+            <div class="container"> --}}
+                <section id="about-section">
+                <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                {{-- <div class="row "> --}}
+                <div class="col-12 mb-4 position-relative">
+                    
+                  <h2 class="section-title">Tentang Kami</h2>
+                </div>
+                <div class="col-lg-4">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illo nesciunt nisi atque corporis placeat nostrum quod magni consectetur commodi aut, quibusdam quam in tenetur ab, officia ratione delectus quaerat!</p>
+      
+                  <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, deserunt tenetur similique exercitationem officiis perspiciatis doloremque deleniti corrupti iste expedita atque odit commodi dolore qui maiores mollitia accusamus dolorum consequatur?</p>
+                </div>
+                <div class="col-lg-4">
+                  <img src="{{asset('img/ptindopara.png')}}" alt="Image" class="img-fluid" width="500px">
+                </div>
+                <div class="col-lg-4">
+                  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+      Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
+      </p>
+      
+                  <p>Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+      Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.</p>
+                </div>
+                
+                
+              {{-- </div> --}}
+            </div>
+          </div>
+
+<!--END About-->
+
+<!--Service-->
+
+<!--End Service-->
+
+
+
         <!-- Content section 1-->
         <section id="scroll">
             <div class="container px-5">
@@ -90,13 +132,70 @@
                 </div>
             </div>
         </section>
+
         <!-- Footer-->
-        <footer class="py-5 bg-black">
+
+        <footer class="py-5 bg-black" id="footer">
+            <div class="container">
+              <div class="row mb-5">
+                <div class="col-md-3">
+                  <h3 class="footer-title">Alamat</h3>
+                  <p><span class="d-inline-block d-md-block">Office Tower A Lantai 9,</span>Mega Kemayoran</p>
+                    <p><span class="d-inline-block d-md-block">Jl. Angkasa Kav.B-6,</span>Kota Baru Bandar Kemayoran, Jakarta Pusat. 10160<p>
+                </div>
+                <div class="col-md-5 mx-auto">
+                  <div class="row">
+                    <div class="col-lg-4">
+                      <h3 class="footer-title"></h3>
+                      <ul class="list-unstyled">
+                        {{-- <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li> --}}
+                      </ul>
+                    </div>
+                    <div class="col-lg-4">
+                      <h3 class="footer-title"></h3>
+                      <ul class="list-unstyled">
+                        {{-- <li><a href="#">Social Marketing</a></li>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Web Development</a></li> --}}
+                      </ul>
+                    </div>
+                    <div class="col-lg-4">
+                      <h3 class="footer-title"></h3>
+                      <ul class="list-unstyled">
+                        {{-- <li><a href="#">Illustration</a></li>
+                        <li><a href="#">Video Editing</a></li>
+                        <li><a href="#">Copywriting</a></li> --}}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <h3 class="footer-title">Kontak Kami</h3>
+                  {{-- <a href="" class="social-circle m-2"><span class="icon-twitter"></span></a>
+                  <a href="" class="social-circle m-2"><span class="icon-facebook"></span></a>
+                  <a href="" class="social-circle m-2"><span class="icon-instagram"></span></a> --}}
+                  <!-- <a href="#" class="social-circle m-2"><span class="icon-dribbble"></span></a>
+                  <a href="#" class="social-circle m-2"><span class="icon-linkedin"></span></a> -->
+                </div>
+              </div>
+      
+              <div class="row">
+                <div class="col-12 text-center">
+                  
+                </div>
+              </div>
+            </div>
+          </footer>
+        {{-- <footer class="py-5 bg-black">
             <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Your Website 2021</p></div>
-        </footer>
+        </footer> --}}
+
+
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="new/js/scripts.js"></script>
+        
     </body>
 </html>
