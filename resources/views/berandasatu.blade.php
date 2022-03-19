6277fb8a5a9756c6b63bc9762ca2dfc27298549a
naufalscess <56064794+naufalscess@users.noreply.github.com>
2022-03-19 19:41:41 +0700

    update section product
    Adding image, setting css

--- resources/views/berandasatu.blade.php
+++ resources/views/berandasatu.blade.php
@@ -13,6 +13,7 @@
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 <body>
 <div class="nav">
@@ -41,8 +42,31 @@
 </div>
 <div class="section3">
      <h1 id="product">PRODUCT</a>
-     <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+     <div class="container">
+            <div class="gallery" >
+                <figure class="gallery__item gallery__item--1" >
+                    <img src="new/img/alakran.jpg" alt="Gallery image 1" class="gallery__img" >
+                    <p>Bawah Kiri<p>
+                </figure>
+                <figure class="gallery__item gallery__item--2">
+                    <img src="new/img/producttank.jpg" alt="Gallery image 2" class="gallery__img">
+                    <h2>tengah atas<h2>
+                </figure>
+                <figure class="gallery__item gallery__item--3">
+                    <img src="new/img/tankhigh.jpg" alt="Gallery image 3" class="gallery__img">
+                    <h3>kanan atas<h3>
+                </figure>
+                <figure class="gallery__item gallery__item--4">
+                    <img src="new/img/observer.jpg" alt="Gallery image 4" class="gallery__img">
+                    <h4>bawah kanan</h4>
+                </figure>
+            </div>
+        </div>
+
+
 </div>
+     <!-- <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p> -->
+
 <div class="section4">
      <h1 id="contact">CONTACT</a>
      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
@@ -50,6 +74,5 @@
 </div>
 <script src="node_modules/jquery/dist/jquery.js"></script>
 <script src="js/scripts.js"></script>
-
 </body>
 </html>
