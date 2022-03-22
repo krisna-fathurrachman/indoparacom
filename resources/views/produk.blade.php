15b6ecb6b912e6dfa68f17fbf68028f3211e0bcc
Anggiawanprasetyo <aanggiawan92@gmail.com>
2022-03-22 21:36:45 +0700

    Update produk.blade.php

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -1,1 +1,97 @@
-<h1>ISINYA PRODUK LENGKAP</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> E-Commerce</title>
+    <link rel="stylesheet" href="new/css/produk.css">
+    <link href="new/css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div class="container px-5">
+            <img src="assets/img/logo.png" alt="logo" style="width:50px;height:50px;">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                <a class="nav-link" href="/newberanda">Beranda</a>
+              <a class="nav-link" href="/newberanda#about-section">Tentang Kami</a>
+              <a class="nav-link" href="/newberanda#list-produk">Produk</a>
+              <a class="nav-link" href="/newberanda#team">Team</a>
+              <a class="nav-link" href="/newberanda#footer">Kontak</a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+     
+    <div class="badan">
+        <h2>Kedaraan Militer</h2>
+ 
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+ 
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+    </div> 
+
+    <div class="badan">
+        <h2>Senjata</h2>
+ 
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Alakran</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+ 
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+
+        <div class="list-produk">
+            <img src="new/img/producttank.jpg" alt="tank">
+            <h4>Tank Arisgator</h4>
+        </div>
+    </div> 
+ 
+</body> 
+</html> 
