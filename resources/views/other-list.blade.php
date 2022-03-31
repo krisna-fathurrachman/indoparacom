3735b8a735becfde59535df9c12772e32fb8585a
naufalscess <56064794+naufalscess@users.noreply.github.com>
2022-04-01 00:11:22 +0700

    Update other-list.blade.php
    memperbarui tampilan other list

--- resources/views/other-list.blade.php
+++ resources/views/other-list.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="page_type" content="np-template-header-footer-from-plugin">
     <title>other-list</title>
-    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="css/nicepage1.css" media="screen">
 <link rel="stylesheet" href="css/other-list.css" media="screen">
     <script class="u-script" type="text/javascript" src="js/jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
@@ -16,45 +16,45 @@
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     
     
+    
+    
+    
+    
     <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": ""
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    "name": ""
 }</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="other-list">
     <meta property="og:type" content="website">
   </head>
-  <body class="u-body u-xl-mode">
-    <section class="u-black u-clearfix u-section-1" id="sec-a9d8">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-container-style u-group u-group-1">
-          <div class="u-container-layout">
-            <a href="https://nicepage.com/joomla-templates" class="u-border-1 u-border-active-palette-2-base u-border-hover-palette-1-base u-btn u-button-style u-custom-font u-none u-text-custom-color-1 u-btn-1">HOME</a><span class="u-file-icon u-icon u-text-custom-color-1 u-icon-1" data-href="Home.html" data-page-id="1655921"><img src="images/1.png" alt=""></span>
-          </div>
+  <body class="u-body u-xl-mode"><header class="u-black u-clearfix u-header u-header" id="sec-345c"><div class="u-clearfix u-sheet u-valign-middle-xl u-sheet-1"><span class="u-file-icon u-icon u-text-custom-color-1 u-icon-1"><img src="images/1.png" alt=""></span>
+        <p class="u-custom-font u-text u-text-custom-color-1 u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xs u-text-1">
+          <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-custom-font u-hover-none u-none u-text-custom-color-1 u-btn-1" href="Home.html" data-page-id="1655921">Home </a>
+        </p>
+      </div></header>
+    <section class="u-black u-clearfix u-valign-middle-xs u-section-1" id="sec-4dff">
+      <div class="u-clearfix u-sheet u-valign-top-xl u-sheet-1">
+        <h3 class="u-text u-text-custom-color-1 u-text-default u-text-1">Other List</h3>
+        <div class="u-border-3 u-border-custom-color-1 u-line u-line-horizontal u-line-1"></div>
+        <div class="u-expanded-width-xs u-shape u-shape-svg u-text-custom-color-1 u-shape-1">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 150" style=""><use xlink:href="#svg-61af"></use></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 160 150" x="0px" y="0px" id="svg-61af"><path d="M43.2,126.9c14.2,1.3,27.6,7,39.1,15.6c8.3,6.1,19.4,10.3,32.7,5.3c11.7-4.4,18.6-17.4,21-30.2c2.6-13.3,8.1-25.9,15.7-37.1
+  c8.3-12.1,10.8-27.9,5.3-42.7C150.5,20.3,134.6,9,117,7.6C107.9,6.9,98.8,5,90.1,1.9C83-0.6,75-0.7,67.4,2.1
+  c-9.9,3.7-17,11.6-20.1,21c-3.3,10.1-10.9,18-20.6,22.2c-0.1,0-0.1,0.1-0.2,0.1c-20.3,8.9-31,32-24.6,53.2
+  C6.9,115.6,25.2,125.2,43.2,126.9z"></path></svg>
         </div>
-        <h1 class="u-text u-text-custom-color-1 u-text-default u-title u-text-1">Other List</h1>
-        <div class="u-align-left u-border-3 u-border-custom-color-1 u-line u-line-horizontal u-line-1"></div>
-        <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+        <div class="u-clearfix u-expanded-width-lg u-expanded-width-md u-expanded-width-xl u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
-            <div class="u-layout-col">
-              <div class="u-size-30">
-                <div class="u-layout-row">
-                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-30 u-image-1" src="" data-image-width="1072" data-image-height="758">
-                    <div class="u-container-layout u-valign-middle u-container-layout-2"></div>
-                  </div>
-                  <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
-                    <div class="u-container-layout u-valign-middle u-container-layout-3">
-                      <h2 class="u-text u-text-2">Front Observer Manpack</h2>
-                    </div>
-                  </div>
-                </div>
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-image u-image-contain u-layout-cell u-left-cell u-size-28 u-image-1" data-image-width="1072" data-image-height="758">
+                <div class="u-container-layout u-container-layout-1"></div>
               </div>
-              <div class="u-size-30">
-                <div class="u-layout-row">
-                  <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-60 u-layout-cell-3">
-                    <div class="u-container-layout u-container-layout-4"></div>
-                  </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-32 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <h1 class="u-text u-text-default u-text-2">Front Observer Manpack</h1>
+                  <a href="#sec-0116" class="u-border-2 u-border-custom-color-1 u-btn u-button-style u-dialog-link u-hover-custom-color-1 u-none u-text-body-alt-color u-text-hover-white u-btn-1">learn more</a>
                 </div>
               </div>
             </div>
@@ -62,6 +62,10 @@
         </div>
       </div>
     </section>
+    
+    
+    
+    
     
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-f729"><div class="u-clearfix u-sheet u-sheet-1">
@@ -78,5 +82,223 @@
         <span>Website Builder</span>
       </a>. 
     </section>
-  </body>
+  <section class="u-align-center u-black u-clearfix u-container-style u-dialog-block u-opacity u-opacity-70 u-valign-middle-lg u-valign-middle-xl u-dialog-section-7" id="sec-0116">
+      <div class="u-align-left u-container-style u-dialog u-shape-rectangle u-white u-dialog-1">
+        <div class="u-container-layout u-valign-top-xs u-container-layout-1">
+          <div class="u-carousel u-expanded-width-xs u-gallery u-layout-thumbnails u-lightbox u-no-transition u-show-text-always u-gallery-1" id="carousel-6fa7" data-interval="5000" data-u-ride="carousel">
+            <div class="u-carousel-inner u-gallery-inner" role="listbox">
+              <div class="u-active u-carousel-item u-gallery-item u-carousel-item-1">
+                <div class="u-back-slide" data-image-width="708" data-image-height="526">
+                  <img class="u-back-image u-expanded u-image-contain" src="images/observer.jpg">
+                </div>
+                <div class="u-over-slide u-over-slide-1">
+                  <h3 class="u-gallery-heading">Sample Title</h3>
+                  <p class="u-gallery-text">Sample Text</p>
+                </div>
+              </div>
+              <div class="u-carousel-item u-gallery-item u-carousel-item-2">
+                <div class="u-back-slide" data-image-width="725" data-image-height="489">
+                  <img class="u-back-image u-expanded u-image-contain" src="images/observer2.jpg">
+                </div>
+                <div class="u-over-slide u-over-slide-2">
+                  <h3 class="u-gallery-heading">Sample Title</h3>
+                  <p class="u-gallery-text">Sample Text</p>
+                </div>
+              </div>
+              <div class="u-carousel-item u-gallery-item u-carousel-item-3" data-image-width="2836" data-image-height="1875">
+                <div class="u-back-slide" data-image-width="713" data-image-height="402">
+                  <img class="u-back-image u-expanded u-image-contain" src="images/Capture.jpg">
+                </div>
+                <div class="u-over-slide u-over-slide-3">
+                  <h3 class="u-gallery-heading">Sample Title</h3>
+                  <p class="u-gallery-text">Sample Text</p>
+                </div>
+              </div>
+            </div>
+            <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-1" href="#carousel-6fa7" role="button" data-u-slide="prev">
+              <span aria-hidden="true">
+                <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+              </span>
+              <span class="sr-only">
+                <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+              </span>
+            </a>
+            <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-2" href="#carousel-6fa7" role="button" data-u-slide="next">
+              <span aria-hidden="true">
+                <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+              </span>
+              <span class="sr-only">
+                <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+              </span>
+            </a>
+            <ol class="u-carousel-thumbnails u-spacing-10 u-carousel-thumbnails-1">
+              <li class="u-active u-carousel-thumbnail u-carousel-thumbnail-1" data-u-target="#carousel-6fa7" data-u-slide-to="0">
+                <img class="u-carousel-thumbnail-image u-image" src="images/observer.jpg">
+              </li>
+              <li class="u-carousel-thumbnail u-carousel-thumbnail-2" data-u-target="#carousel-6fa7" data-u-slide-to="1">
+                <img class="u-carousel-thumbnail-image u-image" src="images/observer2.jpg">
+              </li>
+              <li class="u-carousel-thumbnail u-carousel-thumbnail-3" data-u-target="#carousel-6fa7" data-u-slide-to="2">
+                <img class="u-carousel-thumbnail-image u-image" src="images/Capture.jpg">
+              </li>
+            </ol>
+          </div>
+        </div><button class="u-dialog-close-button u-icon u-text-grey-50 u-icon-1">
+        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 409.806 409.806" style=""><use xlink:href="#svg-4e2d"></use></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 409.806 409.806" x="0px" y="0px" id="svg-4e2d" style="enable-background:new 0 0 409.806 409.806;"><g><g><path d="M228.929,205.01L404.596,29.343c6.78-6.548,6.968-17.352,0.42-24.132c-6.548-6.78-17.352-6.968-24.132-0.42    c-0.142,0.137-0.282,0.277-0.42,0.42L204.796,180.878L29.129,5.21c-6.78-6.548-17.584-6.36-24.132,0.42    c-6.388,6.614-6.388,17.099,0,23.713L180.664,205.01L4.997,380.677c-6.663,6.664-6.663,17.468,0,24.132    c6.664,6.662,17.468,6.662,24.132,0l175.667-175.667l175.667,175.667c6.78,6.548,17.584,6.36,24.132-0.42    c6.387-6.614,6.387-17.099,0-23.712L228.929,205.01z"></path>
+</g>
+</g></svg>
+      </button>
+      </div>
+    </section><style> .u-dialog-section-7 {
+  min-height: 826px;
+}
+
+.u-dialog-section-7 .u-dialog-1 {
+  width: 800px;
+  min-height: 620px;
+  background-image: none;
+  margin: 60px auto;
+}
+
+.u-dialog-section-7 .u-container-layout-1 {
+  padding: 45px 23px 30px;
+}
+
+.u-dialog-section-7 .u-gallery-1 {
+  width: 747px;
+  height: 545px;
+  grid-gap: 10px;
+  margin: 0 0 0 auto;
+}
+
+.u-dialog-section-7 .u-carousel-item-1 {
+  margin-right: 0;
+}
+
+.u-dialog-section-7 .u-over-slide-1 {
+  min-height: 100px;
+  padding: 10px;
+}
+
+.u-dialog-section-7 .u-carousel-item-2 {
+  margin-right: 0;
+}
+
+.u-dialog-section-7 .u-over-slide-2 {
+  min-height: 100px;
+  padding: 10px;
+}
+
+.u-dialog-section-7 .u-carousel-item-3 {
+  margin-right: 0;
+}
+
+.u-dialog-section-7 .u-over-slide-3 {
+  min-height: 100px;
+  padding: 10px;
+}
+
+.u-dialog-section-7 .u-carousel-thumbnails-1 {
+  padding-top: 10px;
+}
+
+.u-dialog-section-7 .u-carousel-thumbnail-1 {
+  width: 100px;
+  height: 100px;
+}
+
+.u-dialog-section-7 .u-carousel-thumbnail-2 {
+  width: 100px;
+  height: 100px;
+}
+
+.u-dialog-section-7 .u-carousel-thumbnail-3 {
+  width: 100px;
+  height: 100px;
+}
+
+.u-dialog-section-7 .u-carousel-control-1 {
+  position: absolute;
+  left: 10px;
+  width: 40px;
+  height: 40px;
+}
+
+.u-dialog-section-7 .u-carousel-control-2 {
+  position: absolute;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+}
+
+.u-dialog-section-7 .u-icon-1 {
+  width: 16px;
+  height: 16px;
+  left: auto;
+  top: 14px;
+  position: absolute;
+  right: 14px;
+}
+
+@media (max-width: 991px) {
+  .u-dialog-section-7 .u-dialog-1 {
+    width: 720px;
+    height: auto;
+    margin-top: 138px;
+  }
+
+  .u-dialog-section-7 .u-container-layout-1 {
+    padding-top: 29px;
+  }
+
+  .u-dialog-section-7 .u-gallery-1 {
+    width: 675px;
+    margin-right: 0;
+  }
+}
+
+@media (max-width: 767px) {
+  .u-dialog-section-7 .u-dialog-1 {
+    width: 540px;
+    margin-top: 124px;
+  }
+
+  .u-dialog-section-7 .u-container-layout-1 {
+    padding-top: 45px;
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+
+  .u-dialog-section-7 .u-gallery-1 {
+    width: 520px;
+  }
+}
+
+@media (max-width: 575px) {
+  .u-dialog-section-7 .u-dialog-1 {
+    width: 340px;
+    margin-top: 33px;
+    margin-bottom: 57px;
+  }
+
+  .u-dialog-section-7 .u-container-layout-1 {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .u-dialog-section-7 .u-gallery-1 {
+    margin-right: initial;
+    margin-left: initial;
+    width: auto;
+  }
+}</style></body>
 </html>
