8bd62e7f8bcd3779a1a63566924ad00b3324f7a8
naufalscess <56064794+naufalscess@users.noreply.github.com>
2022-04-08 05:48:06 +0700

    Update other-list.blade.php dan other-list.css
    merubah tampilan other list

--- resources/views/other-list.blade.php
+++ resources/views/other-list.blade.php
@@ -3,65 +3,73 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <meta name="keywords" content="Tank Arisgator, Radio Repeater, Vehicle Personnel, Maintenance Truck">
+    <meta name="keywords" content="Other List, Front Observer Manpack">
     <meta name="description" content="">
     <meta name="page_type" content="np-template-header-footer-from-plugin">
-    <title>other-list</title>
-    <link rel="stylesheet" href="css/nicepage1.css" media="screen">
+    <title>Other List</title>
+    <link rel="stylesheet" href="css/nicepage3.css" media="screen">
 <link rel="stylesheet" href="css/other-list.css" media="screen">
     <script class="u-script" type="text/javascript" src="js/jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 4.7.8, nicepage.com">
+    <meta name="generator" content="Nicepage 4.8.2, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-
-    
-    
-    
     
     
     <script type="application/ld+json">{
     "@context": "http://schema.org",
     "@type": "Organization",
-    "name": ""
+    "name": "",
+    "logo": "images/default-logo.png"
 }</script>
     <meta name="theme-color" content="#478ac9">
-    <meta property="og:title" content="other-list">
+    <meta property="og:title" content="Page 4">
     <meta property="og:type" content="website">
   </head>
-  <body class="u-body u-xl-mode"><header class="u-black u-clearfix u-header u-header" id="sec-345c"><div class="u-clearfix u-sheet u-valign-middle-xl u-sheet-1"><span class="u-file-icon u-icon u-text-custom-color-1 u-icon-1"><img src="images/1.png" alt=""></span>
-        <p class="u-custom-font u-text u-text-custom-color-1 u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xs u-text-1">
-          <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-custom-font u-hover-none u-none u-text-custom-color-1 u-btn-1" href="/index" data-page-id="1655921">Home </a>
-        </p>
-      </div></header>
-    <section class="u-black u-clearfix u-valign-middle-xs u-section-1" id="sec-4dff">
-      <div class="u-clearfix u-sheet u-valign-top-xl u-sheet-1">
-        <h3 class="u-text u-text-custom-color-1 u-text-default u-text-1">Other List</h3>
-        <div class="u-border-3 u-border-custom-color-1 u-line u-line-horizontal u-line-1"></div>
-        <div class="u-expanded-width-xs u-shape u-shape-svg u-text-custom-color-1 u-shape-1">
-          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 150" style=""><use xlink:href="#svg-61af"></use></svg>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 160 150" x="0px" y="0px" id="svg-61af"><path d="M43.2,126.9c14.2,1.3,27.6,7,39.1,15.6c8.3,6.1,19.4,10.3,32.7,5.3c11.7-4.4,18.6-17.4,21-30.2c2.6-13.3,8.1-25.9,15.7-37.1
-  c8.3-12.1,10.8-27.9,5.3-42.7C150.5,20.3,134.6,9,117,7.6C107.9,6.9,98.8,5,90.1,1.9C83-0.6,75-0.7,67.4,2.1
-  c-9.9,3.7-17,11.6-20.1,21c-3.3,10.1-10.9,18-20.6,22.2c-0.1,0-0.1,0.1-0.2,0.1c-20.3,8.9-31,32-24.6,53.2
-  C6.9,115.6,25.2,125.2,43.2,126.9z"></path></svg>
-        </div>
-        <div class="u-clearfix u-expanded-width-lg u-expanded-width-md u-expanded-width-xl u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
-            <div class="u-layout-row">
-              <div class="u-align-left u-container-style u-image u-image-contain u-layout-cell u-left-cell u-size-28 u-image-1" data-image-width="1072" data-image-height="758">
-                <div class="u-container-layout u-container-layout-1"></div>
-              </div>
-              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-32 u-layout-cell-2">
-                <div class="u-container-layout u-container-layout-2">
-                  <h1 class="u-text u-text-default u-text-2">Front Observer Manpack</h1>
-                  <a href="#sec-0116" class="u-border-2 u-border-custom-color-1 u-btn u-button-style u-dialog-link u-hover-custom-color-1 u-none u-text-body-alt-color u-text-hover-white u-btn-1">learn more</a>
-                </div>
+  <body class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-b812"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1">
+          <img src="images/default-logo.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-1.html" style="padding: 10px 20px;">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-2.html" style="padding: 10px 20px;">Page 2</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-3.html" style="padding: 10px 20px;">Page 3</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-4.html" style="padding: 10px 20px;">Page 4</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-1.html">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-2.html">Page 2</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-3.html">Page 3</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-4.html">Page 4</a>
+</li></ul>
               </div>
             </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
           </div>
-        </div>
+        </nav>
+      </div></header>
+    <section class="u-black u-clearfix u-section-1" id="sec-f164">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <img class="u-expanded-width-xs u-image u-image-default u-image-1" src="images/frontobservermanpack-removebg-preview.png" alt="" data-image-width="594" data-image-height="420">
+        <h2 class="u-text u-text-custom-color-1 u-text-default u-text-1">Other List</h2>
+        <div class="u-border-3 u-border-custom-color-1 u-line u-line-horizontal u-line-1"></div>
+        <h2 class="u-text u-text-default u-text-2">Front Observe​r ​​Manpack</h2>
       </div>
     </section>
     
--- resources/views/weapons-list.blade.php
+++ resources/views/weapons-list.blade.php
@@ -9,8 +9,8 @@
     <title>Weapons List</title>
     <link rel="stylesheet" href="css/nicepage3.css" media="screen">
 <link rel="stylesheet" href="css/Weapons-list.css" media="screen">
-    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 4.8.2, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
